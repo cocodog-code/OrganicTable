d17d638c0f932cc68eb191dc23117c281a62922f
Naoya Ozawa <naoya.ozawa.y@gmail.com>
2020-09-13 21:51:37 +0900

    Add a title test with a constant #3

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
+  render_views
+
+  before do
+    get :about
+  end
 
   describe "GET #about" do
     it "returns http success" do
-      get :about
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(200)
+    end
+
+    it 'has the right title' do
+      assert_select "title", "About | #{Constants::BASE_TITLE_NAME}"
     end
   end
 
