fb7b2bdbe4a246b08bb3191a840dd7f150f694fc
cocodog-code <62121392+cocodog-code@users.noreply.github.com>
2020-09-15 14:12:13 +0900

    Revert "Create about page#3"

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-  def full_title(page_title: '')
-    if page_title.empty?
-      Constants::BASE_TITLE_NAME
-    else
-      "#{page_title} | #{Constants::BASE_TITLE_NAME}"
-    end
-  end
 end
