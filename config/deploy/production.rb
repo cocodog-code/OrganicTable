82a63576d41c330b3fc3edb737356dd87cd8d081
Naoya Ozawa <naoya.ozawa.y@gmail.com>
2020-10-27 10:45:53 +0900

    Fix rubocop errors #2

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,10 +1,9 @@
-server '13.231.42.225',
-   user: "naoya",
-   roles: %w{web db app},
-   ssh_options: {
-      port: 22,
-      user: "naoya", # overrides user setting above
-      keys: %w(~/.ssh/organic_key_rsa),
-      forward_agent: true,
-      auth_methods: %w(publickey)
-   }
+server '13.231.42.225', user: "naoya", roles: %w(web db app)
+
+set :ssh_options, {
+  port: 22,
+  user: "naoya", # overrides user setting above
+  keys: %w(~/.ssh/organic_key_rsa),
+  forward_agent: true,
+  auth_methods: %w(publickey),
+}
