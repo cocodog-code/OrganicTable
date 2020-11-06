a873af945511f53919569fcb6d324301663abaeb
Naoya Ozawa <naoya.ozawa.y@gmail.com>
2020-11-06 15:38:33 +0900

    Finish store model test and validation #26

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,2 +1,9 @@
 class Store < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :genre, presence: true, length: { maximum: 30 }
+  validates :phone, presence: true, length: { maximum: 20 }, uniqueness: true
+  validates :access, presence: true, length: { maximum: 30 }
+  validates :hour, presence: true, length: { maximum: 50 }
+  validates :website, presence: true, length: { maximum: 100 }
+  validates :address, presence: true, length: { maximum: 50 }
 end
