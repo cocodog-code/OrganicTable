e9470816f12122fd6c8fa58c477a4df1eb68d1fb
Naoya Ozawa <naoya.ozawa.y@gmail.com>
2020-11-08 14:58:30 +0900

    Add show action to store controller #28

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,4 +1,8 @@
 class StoresController < ApplicationController
   def new
   end
+
+  def show
+    @store = Store.find(params[:id])
+  end
 end
--- spec/controllers/stores_controller_spec.rb
+++ spec/controllers/stores_controller_spec.rb
@@ -1,12 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe StoresController, type: :controller do
-
   describe "GET #new" do
     it "returns http success" do
       get :new
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status 200
     end
   end
 
+  describe "GET #show" do
+    let(:store) { create(:store) }
+
+    it "returns http success" do
+      get :show, params: { id: store.id }
+      expect(response).to have_http_status 200
+    end
+  end
 end
