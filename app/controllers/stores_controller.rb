f0f5e3fd12058e523ba0da915490db4b2fed9ee5
Naoya Ozawa <naoya.ozawa.y@gmail.com>
2020-11-09 11:40:41 +0900

    Create a brief store registration page #28

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,5 +1,6 @@
 class StoresController < ApplicationController
   def new
+    @store = Store.new
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'stores/new'
   get 'static_pages/about'
   resources :stores
 end
