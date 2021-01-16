ca70f6ce03eb097438123b9475b2d56cbd7b3755
cocodog-code <62121392+cocodog-code@users.noreply.github.com>
2021-01-16 10:57:22 +0900

    Revert "Revert "トップページ作成(レストラン表示画面)""

--- spec/factories/stores.rb
+++ spec/factories/stores.rb
@@ -1,11 +1,14 @@
+include ActionDispatch::TestProcess
+
 FactoryBot.define do
   factory :store do
-    name { 'organictable' }
+    sequence(:name) { |n| "organictable-#{n}" }
     genre { 'Japanese' }
-    phone { '0123-456-789' }
+    sequence(:phone) { |n| "0123-456-789-#{n}" }
     access { '銀座駅から徒歩5分' }
     hour { '10:00 ~ 22:00' }
     website { 'https://example.com' }
     address { '東京都中央区1-2-3' }
+    image { fixture_file_upload(Rails.root.to_s + "/spec/fixtures/files/test_image.jpg") }
   end
 end
